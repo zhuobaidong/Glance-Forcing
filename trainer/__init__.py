11463ba0357d236ed90fbd3a73eb73ceb757ebe9
DongZhuoBai <dzb1120@163.com>
2026-07-25 09:56:51 +0800

    Update __init__.py

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -1,7 +1,7 @@
 from .diffusion import Trainer as DiffusionTrainer
 from .gan import Trainer as GANTrainer
 from .ode import Trainer as ODETrainer
-from .distillation import Trainer as ScoreDistillationTrainer
+from .distillation_lora.py import Trainer as ScoreDistillationTrainer
 from .naive_cd import Trainer as ConsistencyDistillationTrainer
 
 __all__ = [
